3c6e233ab8feba341ddee0717ee31e28618b926d
RodrigoPasaye <cv6200@innovaccion.mx>
2022-09-21 20:42:31 -0500

    Se cambia la cadena de conexion al appsettings y se manda a llamar en el program

--- webApi/webApi/Program.cs
+++ webApi/webApi/Program.cs
@@ -10,7 +10,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddSqlServer<TareasContext>("Data Source=DESKTOP-Q43GDQM\\SQLEXPRESS;Initial Catalog=TareasDb;user id=sa;password=Abcde1");
+//builder.Services.AddSqlServer<TareasContext>("Data Source=DESKTOP-Q43GDQM\\SQLEXPRESS;Initial Catalog=TareasDb;user id=sa;password=Abcde1");
+builder.Services.AddSqlServer<TareasContext>(builder.Configuration.GetConnectionString("ConexionDB"));
 //builder.Services.AddScoped<IHelloWorldService, HelloWorldService>();
 builder.Services.AddScoped<IHelloWorldService>(p => new HelloWorldService());
 builder.Services.AddScoped<ICategoriaService, CategoriaService>();
